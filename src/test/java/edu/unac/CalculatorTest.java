f3097a4caf9d80a6cf6f63acc4991eeeadeb55b3
Vicma98 <vhernandez@unac.edu.co>
2025-11-04 20:24:14 -0500

    modifiction pom-calculatortes

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -1,12 +1,11 @@
 package edu.unac;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class CalculatorTest {
     @Test
     void testAddition(){
-
         Calculator calc = new Calculator();
         assertEquals(5,calc.add(2,3));
     }
@@ -16,4 +15,15 @@ class CalculatorTest {
         assertEquals(6, calc.multiply(2,3));
     }
 
+    @Test
+    void testDivide(){
+        Calculator calc = new Calculator();
+        assertEquals(5,calc.divide(25,5));
+    }
+
+    @Test
+    void testSubstract(){
+        Calculator calc = new Calculator();
+        assertEquals(2, calc.substract(4,2));
+    }
 }
